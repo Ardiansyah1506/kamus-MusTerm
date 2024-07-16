626dbbd8dceeace0cc79e9ba20fd288e8384af97
magang 5 UDI <fauziardiansyah302@gmail.com>
2024-07-16 14:59:37 +0700

    fixx bangett

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "37cea79357da0c1f"; }
+/******/ 		__webpack_require__.h = function() { return "c37aea111e1d8e3b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
