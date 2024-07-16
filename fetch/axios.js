5df3772c47bc9ff3a483d9eae4ba35fc5f762b7d
magang 5 UDI <fauziardiansyah302@gmail.com>
2024-07-16 14:32:56 +0700

    fix banget

--- fetch/axios.js
+++ fetch/axios.js
@@ -1,13 +1,24 @@
 import Axios from "axios";
 
 const axios = Axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
-    headers: {
-        'X-Requested-With': 'XMLHttpRequest',
-        
-    },
-    withCredentials: false,
-    
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  headers: {
+    'X-Requested-With': 'XMLHttpRequest',
+  },
+  withCredentials: false,
 });
 
-export default axios
+axios.interceptors.request.use(request => {
+  console.log('Starting Request', request);
+  return request;
+});
+
+axios.interceptors.response.use(response => {
+  console.log('Response:', response);
+  return response;
+}, error => {
+  console.log('Error:', error);
+  return Promise.reject(error);
+});
+
+export default axios;
