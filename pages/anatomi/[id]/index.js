eef7b8170ab051afdc7c605ac5687d05859555f0
magang 5 UDI <fauziardiansyah302@gmail.com>
2024-07-21 13:50:48 +0700

    update model anatomi

--- pages/anatomi/[id]/index.js
+++ pages/anatomi/[id]/index.js
@@ -1,31 +1,51 @@
+import { useState, useEffect } from 'react';
 import axios from "../../../fetch/axios";
 import Model from "./model";
 import Image from 'next/image';
 
-const Anatomi = ({ anatomi }) => {
+const Patologi = ({ anatomi }) => {
+  const [isModelAvailable, setIsModelAvailable] = useState(false);
+
+  useEffect(() => {
+    const checkModelAvailability = async () => {
+      try {
+        const response = await fetch(`/assets/3D/${anatomi.foto}`);
+        if (response.ok) {
+          setIsModelAvailable(true);
+        } else {
+          setIsModelAvailable(false);
+        }
+      } catch (error) {
+        setIsModelAvailable(false);
+      }
+    };
+
+    if (anatomi && anatomi.foto) {
+      checkModelAvailability();
+    }
+  }, [anatomi]);
+
   if (!anatomi) {
     return (
-      <div className="container mx-auto p-4">Anatomi tidak ditemukan.</div>
+      <div className="container mx-auto p-4">Patologi tidak ditemukan.</div>
     );
   }
-  return (
-    <div className="container mx-auto p-15 text-white ">
-      {anatomi.foto === "tengkorak.glb" ? (
-    <div className="pt-20">
-        <Model name={anatomi.foto}/>
-    </div>
 
+  return (
+    <div className="container mx-auto p-15 text-white">
+      {isModelAvailable ? (
+        <div className="pt-20">
+          <Model name={anatomi.foto} />
+        </div>
       ) : (
         <div className="pt-20 w-full justify-center items-center flex">
-                    <Image
-          src={`/assets/image/${anatomi.foto}`}
-          alt={anatomi.nama}
-          width={300}
-          height={300}
-        />
-    </div>
-    
-
+          <Image
+            src={`/assets/image/${anatomi.foto}`}
+            alt={anatomi.nama}
+            width={300}
+            height={300}
+          />
+        </div>
       )}
       <h1 className="text-xl text-white font-bold">{anatomi.nama}</h1>
       <div
@@ -38,7 +58,6 @@ const Anatomi = ({ anatomi }) => {
 
 export async function getServerSideProps(context) {
   const { id } = context.params;
-
   try {
     const response = await axios.get(`/anatomi/${id}`);
     if (response.data) {
@@ -59,4 +78,4 @@ export async function getServerSideProps(context) {
   };
 }
 
-export default Anatomi;
+export default Patologi;
